73915bcc81459a6988639cf26322fe70e4e3e106
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2026-02-23 10:29:34 -0300

    Refactor quiz app structure: replace StartScreen with Quiz as main entry point, add QuestionScreen, and update UI components.

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  final void Function() startQuiz;
+
+  const StartScreen(this.startQuiz, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        children: [Image.asset('assets/images/quiz-logo.png', width: 300)],
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "Learn flutter by the funny way",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+          ),
+        ],
       ),
     );
   }
