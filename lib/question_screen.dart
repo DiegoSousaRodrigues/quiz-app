5159fe968d2f97be5d0274330b59d182900101c6
DiegoSousaRodrigues <diego.srodrigues2002@gmail.com>
2026-02-23 21:23:46 -0300

    Update QuestionScreen to display dynamic questions and answers from data source

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,20 +12,22 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestions = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'The Question...',
+            currentQuestions.text,
             style: TextStyle(color: Colors.white),
           ),
           SizedBox(height: 30),
-          AnswerButton(text: "Answers: 1", onTap: () {}),
-          AnswerButton(text: "Answers: 2", onTap: () {}),
-          AnswerButton(text: "Answers: 3", onTap: () {}),
-          AnswerButton(text: "Answers: 4", onTap: () {}),
+          AnswerButton(text: currentQuestions.answers[0], onTap: () {}),
+          AnswerButton(text: currentQuestions.answers[1], onTap: () {}),
+          AnswerButton(text: currentQuestions.answers[2], onTap: () {}),
+          AnswerButton(text: currentQuestions.answers[3], onTap: () {}),
         ],
       ),
     );
